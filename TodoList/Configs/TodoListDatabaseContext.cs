b8f72a4f3432bf1777ff7abe50407f63c91aaa09
Louis Li <a0909002250@gmail.com>
2022-04-03 09:03:49 +0800

    調整 - 在DbContext的DbSet上增加virtual修飾詞

--- TodoList/Configs/TodoListDatabaseContext.cs
+++ TodoList/Configs/TodoListDatabaseContext.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+
 using TodoList.Entites;
 
 namespace TodoList.Configs;
 
 public class TodoListDatabaseContext : DbContext
 {
-    public DbSet<TodoTask> TodoTasks { get; set; }
+    public virtual DbSet<TodoTask> TodoTasks { get; set; }
 
     public TodoListDatabaseContext(DbContextOptions<TodoListDatabaseContext> options) : base(options)
     {
